4b560acc7b63d8a1e7495fff9bf37040e7d5d550
BlueAndi <web@blue-andi.de>
2022-12-21 20:40:45 +0100

    Template element tested updated with element name.

--- test/test_unittest.py
+++ test/test_unittest.py
@@ -397,67 +397,67 @@ def test_tmpl_element():
     """
     # Test uint8
     test_value = 1
-    tmpl_element = TmplElement(0, test_value, 8)
+    tmpl_element = TmplElement("test", 0, test_value, 8)
     assert f"0x{test_value:02X}" == tmpl_element.hex()
     assert test_value == tmpl_element
 
     # Test uint16
     test_value = 45000
-    tmpl_element = TmplElement(0, test_value, 16)
+    tmpl_element = TmplElement("test", 0, test_value, 16)
     assert f"0x{test_value:04X}" == tmpl_element.hex()
     assert test_value == tmpl_element
 
     # Test uint32
     test_value = 80000
-    tmpl_element = TmplElement(0, test_value, 32)
+    tmpl_element = TmplElement("test", 0, test_value, 32)
     assert f"0x{test_value:08X}" == tmpl_element.hex()
     assert test_value == tmpl_element
 
     # Test uint64
     test_value = 12345789
-    tmpl_element = TmplElement(0, test_value, 64)
+    tmpl_element = TmplElement("test", 0, test_value, 64)
     assert f"0x{test_value:016X}" == tmpl_element.hex()
     assert test_value == tmpl_element
 
     # Test sint8
     test_value = -1
     value_raw = test_value & ((1 << 8) - 1)
-    tmpl_element = TmplElement(0, test_value, 8)
+    tmpl_element = TmplElement("test", 0, test_value, 8)
     assert f"0x{value_raw:02X}" == tmpl_element.hex()
     assert test_value == tmpl_element
 
     # Test sint16
     test_value = -10000
     value_raw = test_value & ((1 << 16) - 1)
-    tmpl_element = TmplElement(0, test_value, 16)
+    tmpl_element = TmplElement("test", 0, test_value, 16)
     assert f"0x{value_raw:04X}" == tmpl_element.hex()
     assert test_value == tmpl_element
 
     # Test sint32
     test_value = -500000
     value_raw = test_value & ((1 << 32) - 1)
-    tmpl_element = TmplElement(0, test_value, 32)
+    tmpl_element = TmplElement("test", 0, test_value, 32)
     assert f"0x{value_raw:08X}" == tmpl_element.hex()
     assert test_value == tmpl_element
 
     # Test sint64
     test_value = -12345789
     value_raw = test_value & ((1 << 64) - 1)
-    tmpl_element = TmplElement(0, test_value, 64)
+    tmpl_element = TmplElement("test", 0, test_value, 64)
     assert f"0x{value_raw:016X}" == tmpl_element.hex()
     assert test_value == tmpl_element
 
     # Test float32
     test_value = 1.2345
     value_raw = struct.unpack('<I', struct.pack('<f', test_value))[0]
-    tmpl_element = TmplElement(0, test_value, 32)
+    tmpl_element = TmplElement("test", 0, test_value, 32)
     assert f"0x{value_raw:08X}" == tmpl_element.hex()
     assert test_value == tmpl_element
 
     # Test float64
     test_value = 1.2345
     value_raw = struct.unpack('<Q', struct.pack('<d', test_value))[0]
-    tmpl_element = TmplElement(0, test_value, 64)
+    tmpl_element = TmplElement("test", 0, test_value, 64)
     assert f"0x{value_raw:016X}" == tmpl_element.hex()
     assert test_value == tmpl_element
 
@@ -468,7 +468,7 @@ def test_tmpl_element_with_array_part_1():
 
     # Test array of uint8
     test_values = [0, 1, 2, 3]
-    tmpl_element = TmplElement(0, test_values, 8)
+    tmpl_element = TmplElement("test", 0, test_values, 8)
 
     hex_str = "["
     for idx, value in enumerate(test_values):
@@ -483,7 +483,7 @@ def test_tmpl_element_with_array_part_1():
 
     # Test uint16
     test_values = [0, 1, 2, 3]
-    tmpl_element = TmplElement(0, test_values, 16)
+    tmpl_element = TmplElement("test", 0, test_values, 16)
 
     hex_str = "["
     for idx, value in enumerate(test_values):
@@ -498,7 +498,7 @@ def test_tmpl_element_with_array_part_1():
 
     # Test uint32
     test_values = [0, 1, 2, 3]
-    tmpl_element = TmplElement(0, test_values, 32)
+    tmpl_element = TmplElement("test", 0, test_values, 32)
 
     hex_str = "["
     for idx, value in enumerate(test_values):
@@ -513,7 +513,7 @@ def test_tmpl_element_with_array_part_1():
 
     # Test uint64
     test_values = [0, 1, 2, 3]
-    tmpl_element = TmplElement(0, test_values, 64)
+    tmpl_element = TmplElement("test", 0, test_values, 64)
 
     hex_str = "["
     for idx, value in enumerate(test_values):
@@ -533,7 +533,7 @@ def test_tmpl_element_with_array_part_2():
 
     # Test sint8
     test_values = [0, -1, -2, -3]
-    tmpl_element = TmplElement(0, test_values, 8)
+    tmpl_element = TmplElement("test", 0, test_values, 8)
 
     hex_str = "["
     for idx, value in enumerate(test_values):
@@ -549,7 +549,7 @@ def test_tmpl_element_with_array_part_2():
 
     # Test sint16
     test_values = [0, -1, -2, -3]
-    tmpl_element = TmplElement(0, test_values, 16)
+    tmpl_element = TmplElement("test", 0, test_values, 16)
 
     hex_str = "["
     for idx, value in enumerate(test_values):
@@ -565,7 +565,7 @@ def test_tmpl_element_with_array_part_2():
 
     # Test sint32
     test_values = [0, -1, -2, -3]
-    tmpl_element = TmplElement(0, test_values, 32)
+    tmpl_element = TmplElement("test", 0, test_values, 32)
 
     hex_str = "["
     for idx, value in enumerate(test_values):
@@ -581,7 +581,7 @@ def test_tmpl_element_with_array_part_2():
 
     # Test sint64
     test_values = [0, -1, -2, -3]
-    tmpl_element = TmplElement(0, test_values, 64)
+    tmpl_element = TmplElement("test", 0, test_values, 64)
 
     hex_str = "["
     for idx, value in enumerate(test_values):
@@ -602,7 +602,7 @@ def test_tmpl_element_with_array_part_3():
 
     # Test float32
     test_values = [0.123, 1.234, 12.345, 123.456]
-    tmpl_element = TmplElement(0, test_values, 32)
+    tmpl_element = TmplElement("test", 0, test_values, 32)
 
     hex_str = "["
     for idx, value in enumerate(test_values):
@@ -618,7 +618,7 @@ def test_tmpl_element_with_array_part_3():
 
     # Test float64
     test_values = [0.123, 1.234, 12.345, 123.456]
-    tmpl_element = TmplElement(0, test_values, 64)
+    tmpl_element = TmplElement("test", 0, test_values, 64)
 
     hex_str = "["
     for idx, value in enumerate(test_values):
