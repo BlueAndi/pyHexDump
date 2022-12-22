95eaddc0384d7a55cdcd23acaea6e312d2cb62ba
BlueAndi <web@blue-andi.de>
2022-12-22 11:18:42 +0100

    Lower python version required.

--- setup.py
+++ setup.py
@@ -67,13 +67,15 @@ setuptools.setup(
         "Bug Tracker": "https://github.com/BlueAndi/pyHexDump/issues",
     },
     classifiers=[
-        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
     package_dir={"pyHexDump": "src/pyHexDump"},
     packages=setuptools.find_packages(where="src"),
-    python_requires=">=3.9",
+    python_requires=">=3.8",
     include_package_data=True,
     install_requires=[
         "intelhex==2.3.0",
