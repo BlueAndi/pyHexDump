b0cfbf529630fa02f3d060b1799ea4a7e29026b3
BlueAndi <web@blue-andi.de>
2024-03-14 22:50:21 +0100

    Test splitted into different configurations. #9

--- tests/test_print.py
+++ tests/test_print.py
@@ -15,7 +15,7 @@ def test_cmd_registration():
     assert cmd["name"] == "print"
     assert hasattr(cmd["execFunc"], "__call__") is True
 
-def test_call(capsys):
+def test_config(capsys):
     """Test the call via command line args."""
     args = {
         "binaryFile": [ "tests/data/data.txt" ],
@@ -32,10 +32,46 @@ def test_call(capsys):
 
     assert ret_status == Ret.OK
     assert captured_lines[0] == "uint8_single @ 00000000: 49"
-    assert captured_lines[1] == "uint8_array @ 00000000: [49, 50, 51, 52, 53, 54, 55, 56]"
-    assert captured_lines[2] == "utf8 @ 00000000: 12345678"
-    assert captured_lines[3] == "uint8_custom_single.element @ 00000000: 49"
-    assert captured_lines[4] == "uint8_custom_list.element @ 00000000: [49, 50, 51, 52, 53, 54, 55, 56]" #pylint: disable=line-too-long
-    assert captured_lines[5] == "ubyte_list.element.a @ 00000000: 49" #pylint: disable=line-too-long
-    assert captured_lines[6] == "ubyte_list.element.b @ 00000001: 50" #pylint: disable=line-too-long
-    assert captured_lines[7] == ""
+    assert captured_lines[1] == "uint8_array @ 00000001: [50, 51, 52]"
+    assert captured_lines[2] == "utf8 @ 00000004: 567"
+    assert captured_lines[3] == ""
+
+def test_config_structure(capsys):
+    """Test the call via command line args."""
+    args = {
+        "binaryFile": [ "tests/data/data.txt" ],
+        "configFile": [ "tests/data/config_structure.json" ],
+        "templateFile": None,
+        "onlyInHex": False,
+        "verbose": False
+    }
+
+    ret_status = _exec(dict_to_bunch(args))
+
+    captured = capsys.readouterr()
+    captured_lines = captured.out.split("\n")
+
+    assert ret_status == Ret.OK
+    assert captured_lines[0] == "uint8_single_custom.element @ 00000000: 49"
+    assert captured_lines[1] == "uint8_list_custom.element @ 00000000: [49, 50, 51, 52, 53, 54, 55, 56]" #pylint: disable=line-too-long
+    assert captured_lines[2] == ""
+
+def test_config_structure_nested(capsys):
+    """Test the call via command line args."""
+    args = {
+        "binaryFile": [ "tests/data/data.txt" ],
+        "configFile": [ "tests/data/config_structure_nested.json" ],
+        "templateFile": None,
+        "onlyInHex": False,
+        "verbose": False
+    }
+
+    ret_status = _exec(dict_to_bunch(args))
+
+    captured = capsys.readouterr()
+    captured_lines = captured.out.split("\n")
+
+    assert ret_status == Ret.OK
+    assert captured_lines[0] == "ubyte_list.element.a @ 00000000: 49" #pylint: disable=line-too-long
+    assert captured_lines[1] == "ubyte_list.element.b @ 00000001: 50" #pylint: disable=line-too-long
+    assert captured_lines[2] == ""
