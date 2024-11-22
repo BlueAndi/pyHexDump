570ea08ea0e930c0d443421c364940206becee7d
Andreas Merkle <andreas.merkle@newtec.de>
2024-11-22 10:07:50 +0100

    pylint warnings suppressed

--- src/pyHexDump/macros.py
+++ src/pyHexDump/macros.py
@@ -182,7 +182,7 @@ def _read_string(addr, encoding="utf-8"):
 
     return byte_values.decode(encoding)
 
-# pylint: disable-next=too-many-arguments
+# pylint: disable-next=too-many-arguments,too-many-positional-arguments
 def _calc_checksum(binary_data_endianess, start_address, end_address, polynomial, bit_width, seed, \
     reverse_input, reverse_output, final_xor):
 
